075533f727f2feed22659db50ebde9d8c6afd0cd
jian <2277075501@qq.com>
2022-04-16 14:59:46 +0800

    4/16  finish

---
+++ cmake-build-debug/CMakeFiles/15_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_.exe"
+  "15_.exe.manifest"
+  "15_.pdb"
+  "CMakeFiles/15_.dir/15_.cpp.obj"
+  "lib15_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/17.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "17.exe"
+  "17.exe.manifest"
+  "17.pdb"
+  "CMakeFiles/17.dir/17.cpp.obj"
+  "lib17.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
